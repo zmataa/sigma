268aec717598e8b99be2c732f1686e9dbdaea7e7
Zane <zmatarieh@bsd220.org>
2024-10-14 09:36:50 -0500

    Initial Commit

---
+++ sigma/sigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  sigmaApp.swift
+//  sigma
+//
+//  Created by Zane Matarieh on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct sigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sigmaTests/sigmaTests.swift
@@ -0,0 +1,17 @@
+//
+//  sigmaTests.swift
+//  sigmaTests
+//
+//  Created by Zane Matarieh on 10/14/24.
+//
+
+import Testing
+@testable import sigma
+
+struct sigmaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
